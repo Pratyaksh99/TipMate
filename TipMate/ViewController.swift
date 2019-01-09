229adf8941e06d295cec5559897176cab131f9d5
Pratyaksh99 <31634863+Pratyaksh99@users.noreply.github.com>
2019-01-09 18:03:04 -0500

    Initial Commit

---
+++ TipMate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipMate
+//
+//  Created by Pratyaksh Motwani on 1/9/19.
+//  Copyright © 2019 Pratyaksh Motwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
